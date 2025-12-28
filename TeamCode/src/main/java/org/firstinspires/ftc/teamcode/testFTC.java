e21ddba68a9b67c1ed4adae790b9c2c3b126e729
CatalinEmanuelMarcu <catalin_marcu@yahoo.com>
2025-12-28 22:40:09 +0200

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFTC.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Autonomous
 public class testFTC extends OpMode {
