ab2285d7409feb7ad77e85011f38697d6c62087c
CatalinEmanuelMarcu <catalin_marcu@yahoo.com>
2025-12-28 22:25:55 +0200

    Create testFTC.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testFTC.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class testFTC extends OpMode {
+
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    public void loop() {
+        telemetry.addData("Hello", "Cata1");
+    }
+}
